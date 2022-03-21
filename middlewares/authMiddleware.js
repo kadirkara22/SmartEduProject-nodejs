289b319e9e1f9c990903dd89059c49b4670c5947
kadirkara22 <kadirkar2204@gmail.com>
2022-03-21 14:36:23 +0300

    Kurs-teacher-student ilskisi ve search alanları yapıldı

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-const User = require('../modals/User');
+const User = require('../models/User');
 
 module.exports = (req, res, next) => {
     User.findById(req.session.userID, (err, user) => {
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,5 +7,6 @@ const router = express.Router()
 router.route('/').post(roleMiddleWare(["teacher", "admin"]), courseController.createCourse) // http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses)
 router.route('/:slug').get(courseController.getCourse)
-
+router.route('/enroll').post(courseController.enrollCourse)
+router.route('/release').post(courseController.releaseCourse)
 module.exports = router
